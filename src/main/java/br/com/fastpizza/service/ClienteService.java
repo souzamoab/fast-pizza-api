80eabd457479a4ec7415939313097edacac50e02
Moab Souza <souzamoab@gmail.com>
2020-05-24 20:38:55 -0300

    criando interface ClienteService e refatorando classes

--- src/main/java/br/com/fastpizza/service/ClienteService.java
+++ src/main/java/br/com/fastpizza/service/ClienteService.java
@@ -1,26 +1,9 @@
 package br.com.fastpizza.service;
 
 import br.com.fastpizza.entity.Cliente;
-import br.com.fastpizza.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.Optional;
+public interface ClienteService {
 
-@Service
-@CrossOrigin
-public class ClienteService {
-
-    @Autowired
-    private ClienteRepository clienteRepository;
-
-    public Cliente buscar(Integer id) {
-
-        Optional<Cliente> cliente = clienteRepository.findById(id);
-
-        return cliente.orElse(null);
-
-    }
+    Cliente buscar(Integer id);
 
 }
---
+++ src/main/java/br/com/fastpizza/service/ClienteServiceImpl.java
@@ -0,0 +1,21 @@
+package br.com.fastpizza.service;
+
+import br.com.fastpizza.entity.Cliente;
+import br.com.fastpizza.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.orElse(null);
+    }
+}
