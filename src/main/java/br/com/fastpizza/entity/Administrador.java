20a71b800fba1adf307b8e040b0faf39d2e9193a
Moab Souza <souzamoab@gmail.com>
2020-05-23 22:25:26 -0300

    implementando entities Administrador e Cliente

--- src/main/java/br/com/fastpizza/entity/Administrador.java
+++ src/main/java/br/com/fastpizza/entity/Administrador.java
@@ -1,8 +1,13 @@
 package br.com.fastpizza.entity;
 
 import javax.persistence.Entity;
+import java.util.ArrayList;
 
 @Entity
 public class Administrador extends Usuario {
+
+    public Administrador(Integer id, String nome, String cpf, ArrayList<String> telefones, String email, String senha) {
+        super(id, nome, cpf, telefones, email, senha);
+    }
 
 }
--- src/main/java/br/com/fastpizza/entity/Cliente.java
+++ src/main/java/br/com/fastpizza/entity/Cliente.java
@@ -1,5 +1,24 @@
 package br.com.fastpizza.entity;
 
-public class Cliente {
+import javax.persistence.Entity;
+import java.util.ArrayList;
+
+@Entity
+public class Cliente extends Usuario {
+
+    private String rua;
+    private Integer numero;
+    private String bairro;
+    private String pontoReferencia;
+    private String complemento;
+
+    public Cliente(Integer id, String nome, String cpf, ArrayList<String> telefones, String email, String senha, String rua, Integer numero, String bairro, String pontoReferencia, String complemento) {
+        super(id, nome, cpf, telefones, email, senha);
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.pontoReferencia = pontoReferencia;
+        this.complemento = complemento;
+    }
 
 }
