458581a72f65e5b73eb6710b0ce2cb8975abe657
Moab Souza <souzamoab@gmail.com>
2020-05-24 16:07:56 -0300

    criando service de Cliente

---
+++ src/main/java/br/com/fastpizza/service/ClienteService.java
@@ -0,0 +1,11 @@
+package br.com.fastpizza.service;
+
+import br.com.fastpizza.entity.Cliente;
+
+import java.util.Optional;
+
+public interface ClienteService {
+
+    Cliente buscar(Integer id);
+
+}
