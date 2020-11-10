c03503a7aeb4d68b9226efe5ee6f12b8592260f4
Moab Souza <souzamoab@gmail.com>
2020-11-10 16:29:29 -0300

    Entidades e repositories de estado e cidade

---
+++ src/main/java/br/com/fastpizza/repository/CidadeRepository.java
@@ -0,0 +1,9 @@
+package br.com.fastpizza.repository;
+
+import br.com.fastpizza.entity.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
---
+++ src/main/java/br/com/fastpizza/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package br.com.fastpizza.repository;
+
+import br.com.fastpizza.entity.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+}
