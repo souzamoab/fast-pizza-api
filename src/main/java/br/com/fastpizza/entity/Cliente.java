025a121704c7970de1d112a6cb542fcb5ba22a11
Moab Souza <souzamoab@gmail.com>
2020-11-12 22:40:25 -0300

    Ajuste para proteção contra serialização cíclica

--- src/main/java/br/com/fastpizza/entity/Cliente.java
+++ src/main/java/br/com/fastpizza/entity/Cliente.java
@@ -1,6 +1,7 @@
 package br.com.fastpizza.entity;
 
 import br.com.fastpizza.enums.TipoClienteEnum;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -18,6 +19,7 @@ public class Cliente implements Serializable {
     private String cpf;
     private Integer tipo;
 
+    @JsonManagedReference
     @OneToMany(mappedBy = "cliente")
     private List<Endereco> enderecos = new ArrayList<>();
 
