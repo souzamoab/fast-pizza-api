bc32bb1cc89b5ffa8ef984ebabaad758b1cde350
Moab Souza <souzamoab@gmail.com>
2020-11-25 22:14:05 -0300

    Implementação de entidade ItemPedido

--- src/main/java/br/com/fastpizza/entity/Produto.java
+++ src/main/java/br/com/fastpizza/entity/Produto.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 public class Produto implements Serializable {
@@ -27,6 +25,9 @@ public class Produto implements Serializable {
     )
     private List<Categoria> categorias = new ArrayList<>();
 
+    @OneToMany(mappedBy = "id.produto")
+    private Set<ItemPedido> itens = new HashSet<>();
+
     public Produto() {
 
     }
@@ -35,6 +36,14 @@ public class Produto implements Serializable {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
+    }
+
+    public List<Pedido> getPedidos() {
+        List<Pedido> lista = new ArrayList<>();
+        for(ItemPedido i : itens) {
+            lista.add(i.getPedido());
+        }
+        return lista;
     }
 
     public Integer getId() {
@@ -67,6 +76,14 @@ public class Produto implements Serializable {
 
     public void setCategorias(List<Categoria> categorias) {
         this.categorias = categorias;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     @Override
---
+++ src/main/java/br/com/fastpizza/repository/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package br.com.fastpizza.repository;
+
+import br.com.fastpizza.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
